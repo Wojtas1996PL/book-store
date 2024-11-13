fd879e000eb97f134c78f0cdc44bb9a674177da6
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2024-11-13 23:07:53 +0100

    First solution

---
+++ src/main/java/mate/academy/repository/BookRepositoryImpl.java
@@ -0,0 +1,47 @@
+package mate.academy.repository;
+
+import java.util.List;
+import mate.academy.model.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(book);
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save book: " + book);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return book;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        Session session = sessionFactory.openSession();
+        return session.createQuery("select b from Book b", Book.class).getResultList();
+    }
+}
