be8ca64e57fb37170ffb6230c167fd6636a99308
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2024-11-14 18:05:18 +0100

    Second solution

--- src/main/java/mate/academy/model/Book.java
+++ src/main/java/mate/academy/model/Book.java
@@ -1,10 +1,12 @@
 package mate.academy.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
@@ -15,9 +17,14 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
+    @Column(unique = true)
     private String isbn;
+    @NotNull
     private BigDecimal price;
     private String description;
     private String coverImage;
--- src/main/java/mate/academy/service/BookServiceImpl.java
+++ src/main/java/mate/academy/service/BookServiceImpl.java
@@ -8,8 +8,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
     @Autowired
-    private BookRepository bookRepository;
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     @Override
     public Book save(Book book) {
