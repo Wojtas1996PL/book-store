e7c30b5780c210758e3fd036e359803f1a74de1f
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2024-12-10 15:48:28 +0100

    Seventh

--- src/test/java/mate/academy/BookStoreApplicationTests.java
+++ src/test/java/mate/academy/BookStoreApplicationTests.java
@@ -4,12 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-<<<<<<< HEAD
-class BookStoreApplicationTests {
-
-=======
 public class BookStoreApplicationTests {
->>>>>>> main
     @Test
     void contextLoads() {
 
