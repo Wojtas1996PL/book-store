779b4b26b0d291cffec4437feb3f29177d4671e5
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2025-02-06 16:22:42 +0100

    Fixes in liquibase

--- src/main/java/mate/academy/model/CartItem.java
+++ src/main/java/mate/academy/model/CartItem.java
@@ -18,7 +18,7 @@ public class CartItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
-    @JoinColumn(name = "shopping_car_id", nullable = false)
+    @JoinColumn(name = "shopping_cart_id", nullable = false)
     private ShoppingCart shoppingCart;
     @ManyToOne
     @JoinColumn(name = "book_id", nullable = false)
