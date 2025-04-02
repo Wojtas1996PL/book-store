cba1cec9309a4bc297a88489ef96da7f0794c65f
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2025-04-02 16:52:13 +0200

    Add readme - first solution

--- src/main/java/mate/academy/service/UserServiceImpl.java
+++ src/main/java/mate/academy/service/UserServiceImpl.java
@@ -1,11 +1,16 @@
 package mate.academy.service;
 
+import java.util.HashSet;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import mate.academy.dto.user.UserRegistrationRequestDto;
 import mate.academy.dto.user.UserResponseDto;
+import mate.academy.exception.EntityNotFoundException;
 import mate.academy.exception.RegistrationException;
 import mate.academy.mapper.UserMapper;
+import mate.academy.model.Role;
 import mate.academy.model.User;
+import mate.academy.service.repository.role.RoleRepository;
 import mate.academy.service.repository.user.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,6 +21,7 @@ public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
@@ -29,6 +35,11 @@ public class UserServiceImpl implements UserService {
         user.setLastName(requestDto.getLastName());
         user.setShippingAddress(requestDto.getShippingAddress());
         user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        Set<Role> roles = new HashSet<>();
+        Role userRole = roleRepository.findById(1L).orElseThrow(()
+                -> new EntityNotFoundException("User role not found in database"));
+        roles.add(userRole);
+        user.setRoles(roles);
         return userMapper.toUserResponseDto(userRepository.save(user));
     }
 }
